5fad3813bd917706304ab041e86c9f21bd1c08bc
Yash Sondhi <yashsondhi@gmail.com>
2020-04-13 12:17:08 -0400

    reduced time and changed rasflow to non burst node

--- rasflow_test.sh
+++ rasflow_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=rasflow   # Job name
+#SBATCH --job-name=rastest   # Job name
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=yashsondhi@gmail.com     # Where to send mail	
 #SBATCH --ntasks=1                    # Run on a single CPU
 #SBATCH --mem=2gb                     # Job memory request
-#SBATCH --time=12:00:00               # Time limit hrs:min:sec
+#SBATCH --time=4:00:00               # Time limit hrs:min:sec
 #SBATCH --output=serial_test_%j.log   # Standard output and error log
 #SBATCH --qos=kawahara
 
