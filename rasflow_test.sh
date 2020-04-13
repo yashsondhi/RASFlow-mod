6bea4d805e02255ef4275b841158d6fb53aa0115
Yash Sondhi <yashsondhi@gmail.com>
2020-04-13 11:38:39 -0400

    Added test scripts for clsuter

---
+++ rasflow_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=rasflow   # Job name
+#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=yashsondhi@gmail.com     # Where to send mail	
+#SBATCH --ntasks=1                    # Run on a single CPU
+#SBATCH --mem=2gb                     # Job memory request
+#SBATCH --time=12:00:00               # Time limit hrs:min:sec
+#SBATCH --output=serial_test_%j.log   # Standard output and error log
+#SBATCH --qos=kawahara
+
+module load conda
+source activate rasflow
+python main_cl.py --reference "genome" 
+
+
+
