6a7be5002401dada1df6df7cbada9f882a617d36
Yash Sondhi <yashsondhi@gmail.com>
2020-04-13 11:37:25 -0400

    modified script to run on cluster

--- rasflow.sh
+++ rasflow.sh
@@ -3,9 +3,14 @@
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=yashsondhi@gmail.com     # Where to send mail	
 #SBATCH --ntasks=1                    # Run on a single CPU
-#SBATCH --mem=8gb                     # Job memory request
+#SBATCH --mem=50gb                     # Job memory request
 #SBATCH --time=24:00:00               # Time limit hrs:min:sec
 #SBATCH --output=serial_test_%j.log   # Standard output and error log
 #SBATCH --qos=kawahara-b
 
-python main_cl.py --reference genome
+module load conda
+source activate rasflow
+python main_cl.py --reference "genome" 
+
+
+
